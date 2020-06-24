55e961b35683ec3a3a04d20d12b9075f8c7276f1
Long Nguyen <nguyen.long.908132@gmail.com>
2020-06-24 10:57:07 +0700

    Add mingw toolchain entry to build.cpp and vcpkg.cmake

--- toolsrc/src/vcpkg/build.cpp
+++ toolsrc/src/vcpkg/build.cpp
@@ -559,6 +559,10 @@ namespace vcpkg::Build
         {
             return m_paths.scripts / fs::u8path("toolchains/android.cmake");
         }
+		else if (cmake_system_name == "MinGW")
+        {
+            return m_paths.scripts / fs::u8path("toolchains/mingw.cmake");
+        }
         else if (cmake_system_name.empty() || cmake_system_name == "Windows" || cmake_system_name == "WindowsStore")
         {
             return m_paths.scripts / fs::u8path("toolchains/windows.cmake");
